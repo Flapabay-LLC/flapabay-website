6f835318f1c35f0c69928f63bea791d0fe176f34
Fahad Ali <rajpootfahad771@gmail.com>
2025-02-19 18:24:56 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["chart.js"], // This prevents build failure
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
